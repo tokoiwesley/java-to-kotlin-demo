a35a051b4e51967cbf52a8368570730322ab56b0
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-15 23:57:45 +0300

    Add replica of Java code in Kotlin - WIP

--- app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
@@ -5,25 +5,40 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import com.google.android.material.textfield.TextInputLayout
 import com.sriyank.javatokotlindemo.R
-import com.sriyank.javatokotlindemo.activities.MainActivity
 import com.sriyank.javatokotlindemo.app.Constants
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var etName: EditText? = null
-    private var etGithubRepoName: EditText? = null
-    private var etLanguage: EditText? = null
-    private var etGithubUser: EditText? = null
-    private var inputLayoutName: TextInputLayout? = null
-    private var inputLayoutRepoName: TextInputLayout? = null
-    private var inputLayoutGithubUser: TextInputLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
     }
+
+    /**
+     * Sava app username in SharedPreferences
+     */
+    fun saveName(view: View) {}
+
+    /**
+     * Search repositories on github after passing data to DisplayActivity
+     */
+    fun listRepositories(view: View) {
+        val queryRepo = etRepoName.text.toString()
+        val repoLanguage = etLanguage.text.toString()
+
+        val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+        intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+        intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
+        intent.putExtra(Constants.KEY_LANGUAGE, repoLanguage)
+        startActivity(intent)
+    }
+
+    /**
+     * Search repositories of a particular github user after passing data to DisplayActivity
+     */
+    fun listUserRepositories(view: View) {}
 }
