64456f2c7598234be4ec7079a79ff8dbe7cc5b78
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-17 23:43:30 +0300

    Use a companion object

--- app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
@@ -9,6 +9,10 @@ import com.sriyank.javatokotlindemo.app.Constants
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG: String = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
