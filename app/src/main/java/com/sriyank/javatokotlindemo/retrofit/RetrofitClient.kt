dd26163b1ea79cf8521cf96ca3d4dde9e7999f76
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-30 22:46:12 +0300

    Take advantage of default lazy object declaration

--- app/src/main/java/com/sriyank/javatokotlindemo/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/retrofit/RetrofitClient.kt
@@ -6,14 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "https://api.github.com/"
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    val githubAPIService: GithubAPIService by lazy {
-        retrofit.create(GithubAPIService::class.java)
-    }
+    val githubAPIService: GithubAPIService = retrofit.create(GithubAPIService::class.java)
 }
