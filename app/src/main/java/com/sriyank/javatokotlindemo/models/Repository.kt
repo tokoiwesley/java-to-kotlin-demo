7288474036e4ec2de3ec58009d26a68eb7295ce6
Wesley Tokoi <tokoiwesley@gmail.com>
2022-01-05 23:39:17 +0300

    Define primary constructer with serialized property names

--- app/src/main/java/com/sriyank/javatokotlindemo/models/Repository.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/models/Repository.kt
@@ -2,20 +2,14 @@ package com.sriyank.javatokotlindemo.models
 
 import com.google.gson.annotations.SerializedName
 
-class Repository {
-    var id = 0
-    var name: String? = null
-    var language: String? = null
-
-    @SerializedName("html_url")
-    var htmlUrl: String? = null
-    var description: String? = null
-
-    @SerializedName("stargazers_count")
-    var stars: Int? = null
-
-    @SerializedName("watchers_count")
-    var watchers: Int? = null
-    var forks: Int? = null
-    var owner: Owner? = null
-}
+class Repository(
+    var id: Int,
+    var name: String?,
+    var language: String?,
+    @SerializedName("html_url") var htmlUrl: String?,
+    var description: String?,
+    @SerializedName("stargazers_count") var stars: Int?,
+    @SerializedName("watchers_count") var watchers: Int?,
+    var forks: Int?,
+    var owner: Owner?
+)
--- app/src/main/java/com/sriyank/javatokotlindemo/models/SearchResponse.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/models/SearchResponse.kt
@@ -2,8 +2,7 @@ package com.sriyank.javatokotlindemo.models
 
 import com.google.gson.annotations.SerializedName
 
-class SearchResponse {
-    @SerializedName("total_count")
-    var totalCount = 0
-    var items: List<Repository>? = null
-}
+class SearchResponse(
+    @SerializedName("total_count") var totalCount: Int,
+    var items: List<Repository>?
+)
