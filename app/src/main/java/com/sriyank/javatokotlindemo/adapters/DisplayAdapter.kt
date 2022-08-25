e14e22616d9c7adae78c952493bc0135d8678a09
Wesley Tokoi <tokoiwesley@gmail.com>
2022-08-25 10:29:18 +0300

    Make the RecyclerView Adapter code robust and null safe

--- app/src/main/java/com/sriyank/javatokotlindemo/adapters/DisplayAdapter.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/adapters/DisplayAdapter.kt
@@ -6,8 +6,6 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.sriyank.javatokotlindemo.R
 import com.sriyank.javatokotlindemo.adapters.DisplayAdapter.MyViewHolder
@@ -15,53 +13,56 @@ import com.sriyank.javatokotlindemo.app.Util
 import com.sriyank.javatokotlindemo.models.Repository
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class DisplayAdapter(context: Context, items: List<Repository>) :
+class DisplayAdapter(private val context: Context, private var repositoryList: List<Repository>) :
     RecyclerView.Adapter<MyViewHolder>() {
-    private var mData: List<Repository>
-    private val inflater: LayoutInflater
-    private val mContext: Context
+    companion object {
+        private val TAG = DisplayAdapter::class.java.simpleName
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = inflater.inflate(R.layout.list_item, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
         return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val current = mData[position]
+        val current = repositoryList[position]
         holder.setData(current, position)
     }
 
-    override fun getItemCount(): Int {
-        return mData.size
-    }
+    override fun getItemCount(): Int = repositoryList.size
 
-    fun swap(data: List<Repository>) {
-        if (data.size == 0) Util.showMessage(mContext, "No Items Found")
-        mData = data
+    fun swap(dataList: List<Repository>) {
+        if (dataList.isEmpty()) Util.showMessage(context, "No Items Found")
+        repositoryList = dataList
         notifyDataSetChanged()
     }
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private var pos= 0
+        private var pos = 0
         private var current: Repository? = null
 
         init {
             itemView.imgBookmark.setOnClickListener { bookmarkRepository(current) }
             itemView.setOnClickListener {
-                val url = current!!.htmlUrl
-                val webpage = Uri.parse(url)
-                val intent = Intent(Intent.ACTION_VIEW, webpage)
-                if (intent.resolveActivity(mContext.packageManager) != null) {
-                    mContext.startActivity(intent)
+                current?.let {
+                    val url = current!!.htmlUrl
+                    val webpage = Uri.parse(url)
+                    val intent = Intent(Intent.ACTION_VIEW, webpage)
+                    if (intent.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(intent)
+                    }
                 }
             }
         }
 
-        fun setData(current: Repository, position: Int) {
-            itemView.txvName.text = current.name
-            itemView.txvLanguage.text = current.language.toString()
-            itemView.txvForks.text = current.forks.toString()
-            itemView.txvWatchers.text = current.watchers.toString()
-            itemView.txvStars.text = current.stars.toString()
+        fun setData(current: Repository?, position: Int) {
+            current?.let {
+                itemView.txvName.text = current.name
+                itemView.txvLanguage.text = current.language
+                itemView.txvForks.text = current.forks.toString()
+                itemView.txvWatchers.text = current.watchers.toString()
+                itemView.txvStars.text = current.stars.toString()
+            }
             this.pos = position
             this.current = current
         }
@@ -87,15 +88,5 @@ class DisplayAdapter(context: Context, items: List<Repository>) :
 				}
 			});*/
         }
-    }
-
-    companion object {
-        private val TAG = DisplayAdapter::class.java.simpleName
-    }
-
-    init {
-        inflater = LayoutInflater.from(context)
-        mData = items
-        mContext = context
     }
 }
