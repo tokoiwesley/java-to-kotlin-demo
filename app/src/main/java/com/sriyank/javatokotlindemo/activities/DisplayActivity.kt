937e6f90f6dddc06d257dd5d0c0abd1a62e61e09
Wesley Tokoi <tokoiwesley@gmail.com>
2022-10-22 07:20:54 +0300

    Add a duratin default paramater for displaying toast messages

--- app/src/main/java/com/sriyank/javatokotlindemo/activities/DisplayActivity.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/activities/DisplayActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -99,7 +100,7 @@ class DisplayActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
                 }
 
                 override fun onFailure(call: Call<List<Repository>>?, t: Throwable) {
-                    toast(t.message ?: "Error Fetching Results")
+                    toast(t.message ?: "Error Fetching Results", Toast.LENGTH_LONG)
                 }
             })
     }
--- app/src/main/java/com/sriyank/javatokotlindemo/extensions/CommonExtensions.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/extensions/CommonExtensions.kt
@@ -8,16 +8,16 @@ import com.sriyank.javatokotlindemo.models.ErrorResponse
 import okhttp3.ResponseBody
 import java.io.IOException
 
-fun Context.showErrorMessage(errorBody: ResponseBody) {
+fun Context.showErrorMessage(errorBody: ResponseBody, duration: Int = Toast.LENGTH_SHORT) {
     val gson = GsonBuilder().create()
     try {
         val errorResponse = gson.fromJson(errorBody.string(), ErrorResponse::class.java)
-        toast(errorResponse.message!!)
+        toast(errorResponse.message!!, duration)
     } catch (e: IOException) {
         Log.i("Exception ", e.toString())
     }
 }
 
-fun Context.toast(msg: String) {
-    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, msg, duration).show()
 }
