b5d843e8b60c24d5e7d96bf1fac29b27c03dd2fb
Wesley Tokoi <tokoiwesley@gmail.com>
2022-10-31 00:19:10 +0300

    Group all model classes in one file

--- app/src/main/java/com/sriyank/javatokotlindemo/models/Domain.kt
+++ app/src/main/java/com/sriyank/javatokotlindemo/models/Domain.kt
@@ -1,6 +1,7 @@
 package com.sriyank.javatokotlindemo.models
 
 import com.google.gson.annotations.SerializedName
+import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
@@ -15,3 +16,12 @@ open class Repository(
     var forks: Int? = null,
     var owner: Owner? = null
 ) : RealmObject()
+
+open class Owner(var id: Int = 0, var login: String? = null) : RealmObject()
+
+open class SearchResponse(
+    @SerializedName("total_count") var totalCount: Int = 0,
+    var items: RealmList<Repository>? = null
+) : RealmObject()
+
+data class ErrorResponse(val message: String?)
--- app/src/main/java/com/sriyank/javatokotlindemo/models/Owner.kt
+++
@@ -1,5 +0,0 @@
-package com.sriyank.javatokotlindemo.models
-
-import io.realm.RealmObject
-
-open class Owner(var id: Int = 0, var login: String? = null) : RealmObject()
